541e2ab3ffb43e5c56f85c970796ae46d91a596d
svperion <astrorion26@gmail.com>
2022-02-06 23:58:21 -0500

    Fixed the TabView implementation

---
+++ daysJourney macOS/daysJourney_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  daysJourney_macOSApp.swift
+//  daysJourney macOS
+//
+//  Created by Orion Neguse on 2022-01-30.
+//
+
+import SwiftUI
+
+@main
+struct daysJourney_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- daysJourney/ContentView.swift
+++ daysJourney/ContentView.swift
@@ -8,11 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentIndex = 0
+    private let colors: [Color] = [.red, .blue, .green, .yellow]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        TabView() {
+            Text("First")
+            MainPage()
+            Text("Third")
+        }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
     }
 }
+
+
+struct MainPage: View {
+    @State var currentWrite = "Changing Stuff";
+    var body: some View {
+
+        VStack {
+
+            VStack {
+                Text("daysJourney")
+                        .font(.title)
+                        .foregroundColor(.purple)
+                        .padding(.bottom, 10)
+
+
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Sun, Jan 30, 2022 >")
+                            .font(.headline)
+                            .foregroundColor(.gray)
+                })
+            }
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .topLeading)
+                    .padding(.bottom)
+
+            ScrollView {
+                VStack {
+                    Text("7:52 pm:")
+                            .font(.body)
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+
+                    TextEditor(text: $currentWrite)
+                            .font(.body)
+
+                }
+                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, minHeight: 700, maxHeight: .infinity)
+
+            }
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 500, maxHeight: 550, alignment: .topLeading)
+
+
+        }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                .padding(.horizontal)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
